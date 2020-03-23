1163ad0476827c69746d9fde68bb39850ebbca57
Kaleb M <kay@kalebs-air.attlocal.net>
2020-03-23 09:32:16 -0700

    api routes werent working

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -4,77 +4,100 @@ var bodyParser = require("body-parser");
 var apirouter = express.Router();
 
 // list of frineds
-apirouter.get("/api/friends", function(req,res){
-  res.json(friends);
+apirouter.get("/api/friends", function (req, res) {
+    res.json(friends);
 })
 
 
 //  The function to find a new friend
-apirouter.post("/api/friends", function(req, res){
-  console.log("almost there...");
-  var newFriend = req.body;
-  console.log(newFriend);
+apirouter.post("/api/friends", function (req, res) {
+    console.log("almost there...");
+    var newFriend = req.body;
+    var array = req.body.scores
+    console.log(newFriend);
+    console.log("array: ", array)
 
 
 
-// Function for returning the users into an array  var newScore = function(array){
+    // Function for returning the users into an array  var newScore = function(array){
     var newScore = [];
     for (var i = 0; i < array.length; i++) {
-      newScore.push(parseInt(array[i]));
+        newScore.push(parseInt(array[i]));
     }
-    return newScore;
-  }
-//     The function to calculate the diff between two arrays then adding a diff
-  var totalDiff = function(arrA, arrB){
-    delta = 0;
-    for(var i=0; i<arrA.length; i++){
-      delta += Math.abs(arrA[i] - arrB[i]);
+ 
+    var minDiff = 100
+    var matchFriend={
+        name:"",
+        photo:""
     }
-    return delta;
-  }
 
-  function indexOfMin(array) {
-      if (array.length === 0) {
-          return -1;
-      }
+    for (var i = 0; i<friends.length; i++){
+        var scores = friends[i].scores
+        var dif = 0
+        for (var j=0; j<scores.length; j++){
+            dif = dif + (Math.abs(scores[j]- newScore[j]))   
 
-      var min = array[0];
-      var minIndex = 0;
+        }
+        if (dif<minDiff ){
+            matchFriend.name = friends[i].name;
+            matchFriend.photo = friends[i].photo;
+            minDiff = dif
+        }
+    }
 
-      for (var i = 1; i < array.length; i++) {
-          if (array[i] < min) {
-              minIndex = i;
-              min = array[i];
-          }
-      }
+    //     The function to calculate the diff between two arrays then adding a diff
+    // var totalDiff = function (arrA, arrB) {
+    //     delta = 0;
+    //     for (var i = 0; i < arrA.length; i++) {
+    //         delta += Math.abs(arrA[i] - arrB[i]);
+    //     }
+    //     return delta;
 
-      return minIndex;
-  }
+    //     function indexOfMin(array) {
+    //         if (array.length === 0) {
+    //             return -1;
+    //         }
 
-  var newFriendOutcome = newScore(newFriend['scores[]']); 
-  var currentFriendOutcome = [];
-  var differences = [];
+    //         var min = array[0];
+    //         var minIndex = 0;
 
-  for(var i=0; i < friends.length;i++){
-    currentFriendOutcome.push(newScore(friends[i]['scores[]']));
-  }
+    //         for (var i = 1; i < array.length; i++) {
+    //             if (array[i] < min) {
+    //                 minIndex = i;
+    //                 min = array[i];
+    //             }
+    //         }
 
-//  The for loop function that takes differences bewteen current and new friend results  
-  for (var i=0; i < currentFriendOutcome.length; i++){
-    differences.push(totalDiff(newFriendOutcome, currentFriendOutcome[i]));
-  }
-  console.log("Calculating scores...");
+    //         return minIndex;
+    //     }
+
+    //     var newFriendOutcome = newScore(newFriend['scores[]']);
+    //     var currentFriendOutcome = [];
+    //     var differences = [];
+
+    //     for (var i = 0; i < friends.length; i++) {
+    //         currentFriendOutcome.push(newScore(friends[i]['scores[]']));
+    //     }
+
+    //     //  The for loop function that takes differences bewteen current and new friend results  
+    //     for (var i = 0; i < currentFriendOutcome.length; i++) {
+    //         differences.push(totalDiff(newFriendOutcome, currentFriendOutcome[i]));
+    //     }
+    //     console.log("Calculating scores...");
 
 
-  var minFriend = indexOfMin(differences);
-  var matchFriend = friends[minFriend];
-  console.log("Matching...");
-  console.log(matchFriend);
-  
+    //     var minFriend = indexOfMin(differences);
+    //     var matchFriend = friends[minFriend];
+    //     console.log("Matching...");
+    //     console.log(matchFriend);
 
-  friends.push(newFriend);
-  res.json(matchFriend);
-});
+
+        friends.push(newFriend);
+        console.log("matchfriend:", matchFriend)
+        res.json(matchFriend);
+
+    })
+
 
 module.exports = apirouter;
 
--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+
 
 var PORT = process.env.PORT || 8000;
 
- 
-// create application/json parser
-var jsonParser = bodyParser.json()
+
+
  
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.urlencoded({ extended: true }));
  
-// parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }))
- 
-// parse some custom thing into a Buffer
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
- 
-// parse an HTML body into a string
-app.use(bodyParser.text({ type: 'text/html' }))
+app.use(express.json());
 
+var apiRoutes = require("./FriendFinder/app/routing/apiRoutes");
+var htmlRoutes =require("./FriendFinder/app/routing/htmlRoutes");
+app.use(apiRoutes)
+app.use(htmlRoutes)
 // server start up
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
