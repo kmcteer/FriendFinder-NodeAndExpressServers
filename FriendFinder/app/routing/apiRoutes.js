216de49beb90bbd6238805256c4f1d0796b42ad5
Kaleb M <kay@kalebs-air.attlocal.net>
2020-03-22 23:20:31 -0700

    added main code

---
+++ FriendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,82 @@
+var friends = require("../data/friends.js");
+var express = require("express");
+var bodyParser = require("body-parser");
+var apirouter = express.Router();
+
+// list of frineds
+apirouter.get("/api/friends", function(req,res){
+  res.json(friends);
+})
+
+
+//  The function to find a new friend
+apirouter.post("/api/friends", function(req, res){
+  console.log("almost there...");
+  var newFriend = req.body;
+  console.log(newFriend);
+
+
+
+// Function for returning the users into an array  var newScore = function(array){
+    var newScore = [];
+    for (var i = 0; i < array.length; i++) {
+      newScore.push(parseInt(array[i]));
+    }
+    return newScore;
+  }
+//     The function to calculate the diff between two arrays then adding a diff
+  var totalDiff = function(arrA, arrB){
+    delta = 0;
+    for(var i=0; i<arrA.length; i++){
+      delta += Math.abs(arrA[i] - arrB[i]);
+    }
+    return delta;
+  }
+
+  function indexOfMin(array) {
+      if (array.length === 0) {
+          return -1;
+      }
+
+      var min = array[0];
+      var minIndex = 0;
+
+      for (var i = 1; i < array.length; i++) {
+          if (array[i] < min) {
+              minIndex = i;
+              min = array[i];
+          }
+      }
+
+      return minIndex;
+  }
+
+  var newFriendOutcome = newScore(newFriend['scores[]']); 
+  var currentFriendOutcome = [];
+  var differences = [];
+
+  for(var i=0; i < friends.length;i++){
+    currentFriendOutcome.push(newScore(friends[i]['scores[]']));
+  }
+
+//  The for loop function that takes differences bewteen current and new friend results  
+  for (var i=0; i < currentFriendOutcome.length; i++){
+    differences.push(totalDiff(newFriendOutcome, currentFriendOutcome[i]));
+  }
+  console.log("Calculating scores...");
+
+
+  var minFriend = indexOfMin(differences);
+  var matchFriend = friends[minFriend];
+  console.log("Matching...");
+  console.log(matchFriend);
+  
+
+  friends.push(newFriend);
+  res.json(matchFriend);
+});
+
+module.exports = apirouter;
+
+
+
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
 
+// server start up
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
